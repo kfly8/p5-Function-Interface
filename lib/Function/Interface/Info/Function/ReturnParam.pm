a4b9385991a8dd15a86cd1f2bade74ae3f2551dc
kfly8 <kfly@cpan.org>
2019-03-20 21:41:45 +0900

    added Info::Param and Info::ReturnParam pods

--- lib/Function/Interface/Info/Function/ReturnParam.pm
+++ lib/Function/Interface/Info/Function/ReturnParam.pm
@@ -14,3 +14,29 @@ sub type() { $_[0]->{type} }
 sub type_display_name() { $_[0]->type->display_name }
 
 1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Function::Interface::Info::Function::ReturnParam - information about return values of abstract function
+
+=head1 METHODS
+
+=head2 new
+
+Constructor of Function::Interface::Info::Function::ReturnParam. This is usually called at Function::Interface::info.
+
+=head2 type -> Object
+
+Returns type object of the return value, e.g. C<Str>.
+
+=head3 type_display_name -> Str
+
+Returns type display name of the return value
+
+=head1 SEE ALSO
+
+L<Function::Interface::Info>
+

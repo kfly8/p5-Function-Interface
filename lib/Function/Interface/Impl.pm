8d50de6a070511e1f1207585d43a8e837d293cc1
kfly8 <kfly@cpan.org>
2019-06-19 11:28:38 -0400

    Checking in changes prior to tagging of version 0.06.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index ad10a62..465a2dc 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,9 @@ Revision history for Perl extension Function-Interface
    
     {{$NEXT}}
    
    +0.06 2019-06-19T15:28:30Z
    +    - use B::Hooks::EndOfScope instead of CHECK phase #11
    +
     0.05 2019-03-20T23:29:16Z
         - fixed pod. thanks @manwar! #10

--- lib/Function/Interface/Impl.pm
+++ lib/Function/Interface/Impl.pm
@@ -3,7 +3,7 @@ package Function::Interface::Impl;
 use v5.14.0;
 use warnings;
 
-our $VERSION = "0.05";
+our $VERSION = "0.06";
 
 use Class::Load qw(try_load_class is_class_loaded);
 use Scalar::Util qw(blessed);
